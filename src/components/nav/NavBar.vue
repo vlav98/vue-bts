cbc1a87569900db29af53d8c125f701b9484be17
Valentine <valentinelav98@gmail.com>
2020-11-25 17:58:05 +0100

    Added used navbar

---
+++ src/components/nav/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav class="bg-black">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <span class="font-bold h-8 w-8 text-white"> LV </span>
+            </div>
+            <div class="block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <router-link to="/home" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900">Home</router-link>
+
+                <router-link to="/about" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">About</router-link>
+
+                <router-link to="/contact" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">Contact</router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data: () => ({
+    isOpen: false,
+  }),
+  components: {},
+};
+</script>
