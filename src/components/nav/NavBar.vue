c7e97fccf9a3159b98af92d5c228b2c47bae6b89
Valentine <valentinelav98@gmail.com>
2020-11-26 18:14:38 +0100

    WIP Added switch dark mode

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -9,11 +9,38 @@
             </div>
             <div class="block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <router-link to="/home" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900">Home</router-link>
-
-                <router-link to="/about" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">About</router-link>
-
-                <router-link to="/contact" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">Contact</router-link>
+                <router-link
+                  to="/home"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900"
+                  >Home</router-link
+                >
+                <router-link
+                  to="/discography"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
+                  >Discography</router-link
+                >
+                <router-link
+                  to="/about"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
+                  >About</router-link
+                >
+                <router-link
+                  to="/contact"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
+                  >Contact</router-link
+                >
+              </div>
+            </div>
+            <div class="flex-end">
+              <div class="toggle toggle--daynight">
+                <input
+                  type="checkbox"
+                  id="toggle--daynight"
+                  class="toggle--checkbox"
+                />
+                <label class="toggle--btn" for="toggle--daynight"
+                  ><span class="toggle--feature"></span
+                ></label>
               </div>
             </div>
           </div>
@@ -32,3 +59,7 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss">
+
+</style>
