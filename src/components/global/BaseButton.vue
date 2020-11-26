ecb4e2e8a68cd7b5938e285f9da9c55c9e9ba9b7
Valentine <valentinelav98@gmail.com>
2020-11-26 17:28:34 +0100

    Default button

---
+++ src/components/global/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="btn-default disabled:opacity-50">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "base-button",
+  props: {
+    text: String,
+    disabled: Boolean,
+  },
+};
+</script>
+
+<style scoped></style>
