43a0812acd213f86a1e78bc3ce76ce598566be87
Valentine <valentinelav98@gmail.com>
2020-11-26 18:48:26 +0100

    Correction day/night switch

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -9,17 +9,41 @@
             </div>
             <div class="block mx-4">
               <div class="ml-10 flex items-baseline space-x-4">
-                <router-link to="/home" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900">Home</router-link>
-                <router-link to="/discography" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">Discography</router-link>
-                <router-link to="/about" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">About</router-link>
-                <router-link to="/contact" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">Contact</router-link>
+                <router-link
+                  to="/home"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900"
+                  >Home</router-link
+                >
+                <router-link
+                  to="/discography"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
+                  >Discography</router-link
+                >
+                <router-link
+                  to="/about"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
+                  >About</router-link
+                >
+                <router-link
+                  to="/contact"
+                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
+                  >Contact</router-link
+                >
               </div>
             </div>
           </div>
           <div class="mx-4">
             <div class="toggle toggle--daynight">
-              <input type="checkbox" id="toggle--daynight" class="toggle--checkbox" />
-              <label class="toggle--btn" for="toggle--daynight"><span class="toggle--feature"></span></label>
+              <input
+                type="checkbox"
+                id="toggle--daynight"
+                class="toggle--checkbox"
+                v-model="dayModeCheck"
+                @click="changeMode(dayModeCheck)"
+              />
+              <label class="toggle--btn" for="toggle--daynight"
+                ><span class="toggle--feature"></span
+              ></label>
             </div>
           </div>
         </div>
@@ -32,9 +56,34 @@
 export default {
   name: "NavBar",
   data: () => ({
-    isOpen: false,
+    dayModeCheck: null,
   }),
+  mounted() {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      this.dayModeCheck = false;
+      document.querySelector("html").classList.add("dark");
+    } else {
+      this.dayModeCheck = true;
+      document.querySelector("html").classList.remove("dark");
+    }
+  },
   components: {},
+  methods: {
+    changeMode(dayModeCheck) {
+      console.log(dayModeCheck);
+      if(dayModeCheck) {
+        this.dayModeCheck = false;
+        document.querySelector("html").classList.add("dark");
+      } else {
+        this.dayModeCheck = true;
+        document.querySelector("html").classList.remove("dark");
+      }
+    }
+  },
 };
 </script>
 
