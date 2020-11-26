a42de97843353598c10e088652d4f350bb41d3d2
Valentine <valentinelav98@gmail.com>
2020-11-26 16:42:38 +0100

    Clear comments

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,8 @@
 <template>
 <div class="dark:bg-black" id="main-view">
     <NavBar></NavBar>
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold leading-tight text-gray-900">Discographie de BTS</h1>
-    </div>
     <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
-      <!-- <div class="px-4 py-6 sm:px-0">
-        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-      </div>--> 
       <router-view></router-view>
-      <!--<discography-card
-        v-show="datas && discographys.length > 0"
-        v-for="(discography, i) in discographys"
-        :key="i"
-        class="item-list"
-        :class="[i % 2 == 0 ? 'red' : 'blue', discographyPicked(discography)]"
-        :discographyData="discography"
-        :index="i"
-        @add-data="(data) => addOne(data)"
-        @remove-data="(discography) => removeOne(discography)"
-      /> -->
-      <!-- /End replace -->
     </div>
   </div>
 </template>
