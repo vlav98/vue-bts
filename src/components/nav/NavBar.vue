4269731ad3b22b7e63807e6d86b8002ef6c93165
Valentine <valentinelav98@gmail.com>
2020-11-26 18:20:21 +0100

    Correction Navbar elements placements

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,47 +1,25 @@
 <template>
   <div>
     <nav class="bg-black">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="max-w-7xl mx-4 px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
-            <div class="flex-shrink-0">
+            <div class="flex-shrink-0 mx-4">
               <span class="font-bold h-8 w-8 text-white"> LV </span>
             </div>
-            <div class="block">
+            <div class="block mx-4">
               <div class="ml-10 flex items-baseline space-x-4">
-                <router-link
-                  to="/home"
-                  class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900"
-                  >Home</router-link
-                >
-                <router-link
-                  to="/discography"
-                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
-                  >Discography</router-link
-                >
-                <router-link
-                  to="/about"
-                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
-                  >About</router-link
-                >
-                <router-link
-                  to="/contact"
-                  class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700"
-                  >Contact</router-link
-                >
+                <router-link to="/home" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900">Home</router-link>
+                <router-link to="/discography" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">Discography</router-link>
+                <router-link to="/about" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">About</router-link>
+                <router-link to="/contact" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700">Contact</router-link>
               </div>
             </div>
-            <div class="flex-end">
-              <div class="toggle toggle--daynight">
-                <input
-                  type="checkbox"
-                  id="toggle--daynight"
-                  class="toggle--checkbox"
-                />
-                <label class="toggle--btn" for="toggle--daynight"
-                  ><span class="toggle--feature"></span
-                ></label>
-              </div>
+          </div>
+          <div class="mx-4">
+            <div class="toggle toggle--daynight">
+              <input type="checkbox" id="toggle--daynight" class="toggle--checkbox" />
+              <label class="toggle--btn" for="toggle--daynight"><span class="toggle--feature"></span></label>
             </div>
           </div>
         </div>
@@ -60,6 +38,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
