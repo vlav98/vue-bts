b34e08238bd352e836669773f7ce42dfa42bbcb0
Valentine <valentinelav98@gmail.com>
2020-11-29 18:33:57 +0100

    Update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,36 @@
 <template>
   <div>
-    <h1>About</h1>
-    <p>Here is some information. This is the about page</p>
+    <h1 class="text-3xl font-bold leading-tight">About</h1>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 text-justify">
+      <p>Coucou ! Si tu es sur cette page c'est que tu veux en savoir plus sur moi ?</p>
+      <p>Pas de soucis :D</p>
+      <p>Alors par où commencer ?</p>
+      <p>Je suis Valentine Lav. Une étudiante en Master Ingénierie Web et Mobile.</p>
+      <p>En dehors du développement/codage, j'ai d'autres passions. Voici une petite liste :
+      <ul>
+        <li>- Le dessin</li>
+        <li>- La musique (mon groupe favori étant BTS, vous pouvez vous renseigner 
+          <a href="https://bts.ibighit.com/" class="font-bold animate-pulse text-primary">ici</a>)
+        </li>
+        <li>- La résine (vous pouvez trouver quelques uns de mes travaux 
+          <a href="https://www.instagram.com/yilingxin.art/" class="font-bold animate-pulse text-primary">ici</a>)
+        </li>
+        <li>- Et pleins d'autres choses en réalité !</li>
+      </ul></p>
+      <p>Mon site n'est pas parfait, c'est parce que j'apprends !</p>
+      <p>Hmm, pour finir cette page, comment devrais-je faire ... ?</p>
+      <p>Je sais, je vais mettre ma ligne préférée de BTS !</p>
+      <div class="leading-tight animate-pulse ease-in-out text-primary m-6">
+        <div class="text-3xl text-left"> ‟ </div>
+        <div class="text-xl text-center">
+          Laisse-moi te dire à travers cette chanson<br>
+          Les gens disent que le monde a changé<br>
+          Mais heureusement entre toi et moi rien n’a changé<br>
+        </div>
+        <div class="text-3xl text-right"> ” </div>
+      </div>
+      <p>Crédits à : <a href="https://btsarmyfrance.fr/life-goes-on/" class="font-bold text-primary">BTS ARMY FRANCE</a></p>
+    </div>
   </div>
 </template>
 
