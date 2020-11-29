2aca79792248649dd9210d925b9892a81169a6a1
Valentine <valentinelav98@gmail.com>
2020-11-29 17:06:49 +0100

    Tried transition

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -47,7 +47,7 @@
             />
           </svg>
         </button>
-        <div :class="{'flex': isOpen, 'hidden' : !isOpen }" class="top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto duration-150 ease-in-out">
+        <div :class="{'flex': isOpen, 'hidden' : !isOpen }" class="top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto transition duration-150 ease-in-out">
           <div class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
             <router-link to="/home" class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 items-center justify-center ">Home</router-link>
             <router-link to="/discography" class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-md text-sm font-medium text-white hover:text-white hover:bg-gray-700 items-center justify-center ">Discography</router-link>
