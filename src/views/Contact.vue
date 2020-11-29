74ec367bfcf1d84ef5ed0458e49a891ec882e6ab
Valentine <valentinelav98@gmail.com>
2020-11-29 18:41:09 +0100

    Completed pages

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h1>Contact Me</h1>
-  <p>
-    <ul>
-      <li>Email: {{email}}</li>
-      <li>Website: {{web}}</li>
-    </ul>
-  </p>
+    <h1 class="text-3xl font-bold leading-tight text-gray-900 pb-4">Contact Me</h1>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 text-justify">
+    <p>
+      <ul class="text-justify">
+        <li>Email: {{email}}</li>
+        <li>Portfolio(Fr): <a href="https://vlav.carrd.co/" class="text-bold text-primary">{{carrdFr}}</a></li>
+        <li>Portfolio(Eng): <a href="https://vlav-eng.carrd.co/" class="text-bold text-primary">{{carrdEng}}</a></li>
+        <li>Twitter: <a href="https://twitter.com/vlav98" class="text-bold text-primary">{{twitter}}</a></li>
+      </ul>
+    </p>
+    </div>
   </div>
 </template>
 
@@ -15,8 +19,10 @@ export default {
   name: 'Contact',
   data(){
     return {
-      email: 'maegan@maeganwilson.com',
-      web: 'maeganjwilson.github.com'
+      email: 'Peut être que je le mettrais un jour',
+      carrdEng: 'https://vlav-eng.carrd.co/',
+      carrdFr: 'https://vlav.carrd.co/',
+      twitter: 'https://twitter.com/vlav98',
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <p>Il y a pour l'instant peu de contenu.</p>
       <p>Tu peux trouver dans Home cette page.</p>
       <p>Dans Discography, tu pourras trouver toute la discographie du groupe de musique de BTS. D'autres fonctionnalités vont arriver !</p>
-      <p>Si vous voulez en savoir plus sur BTS, je vous invite à regarder <a href="btsarmyfrance.fr" class="animate-pulse text-primary">ce site</a> ! </p>
+      <p>Si vous voulez en savoir plus sur BTS, je vous invite à regarder <a href="btsarmyfrance.fr" class="animate-pulse text-primary font-bold">ce site</a> ! </p>
       <p>Dans About, c'est ce qui est censé être écrit ici hehe mais on verra bien si je rajoute de nouvelles informations :)</p>
       <p>Dans contact, ça sera toutes les infos pour que vous puissiez me contacter ! </p>
       <p>Il faudrait peut-être que j'apprenne à faire un formulaire qui puisse récupérer des messages et qui me les envoie dans ma boîte mail personnelle :D</p>
